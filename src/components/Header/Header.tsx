f33e50a86ff5eff7911954671d6e1e53d51700f8
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2024-05-20 21:09:15 -0300

    Atualizações finais, readme, estilos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,12 +8,11 @@ export function Header(){
                 <img className={styles.logo} src={ marvelLogo } alt="Logo da Marvel" />
 
                 <div className={styles.links}>
-                    <a className={styles.links} href="https://www.linkedin.com/in/viniciusaguiar-araujo/">Personagens</a>
-                    <a className={styles.links} href="https://www.linkedin.com/in/viniciusaguiar-araujo/">Filmes</a>
-                    <a className={styles.links} href="https://www.linkedin.com/in/viniciusaguiar-araujo/">HQ's</a>
+                    <a className={styles.links} href="#">Personagens</a>
+                    <a className={styles.links} href="#">Filmes</a>
+                    <a className={styles.links} href="#">HQ's</a>
                     <a className={styles.links} href="https://www.linkedin.com/in/viniciusaguiar-araujo/">Linkedin</a>
                 </div>
-
                 <div className={styles.profile}>
                     <img className={styles.avatar} src="https://github.com/ViniAguiar1.png" alt="" />
                     <strong className={styles.links}>Sair</strong>
